5f799cfde0ef7006c253c4f013ad4973693e7c2f
Quoc Ty <nguyenquocty080259@gmail.com>
2026-05-22 03:02:23 +0700

    kien truc cua frontend ne

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "../pages/home/HomePage";
+import LoginPage from "../pages/auth/LoginPage";
+import RegisterPage from "../pages/auth/RegisterPage";
+import NotFoundPage from "../pages/error/NotFoundPage";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
