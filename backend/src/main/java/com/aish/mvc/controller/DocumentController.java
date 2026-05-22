0fe5289b72ac48775aa7332f7c305693aa37f4dc
tranvudinhlang2005 <tranvudinhlang2005@gmail.com>
2026-05-22 09:47:02 +0700

    moisua

--- backend/src/main/java/com/aish/mvc/controller/DocumentController.java
+++ backend/src/main/java/com/aish/mvc/controller/DocumentController.java
@@ -1,10 +1,13 @@
 package com.aish.mvc.controller;
 
+import com.aish.mvc.dto.DocumentRequestDTO;
 import com.aish.mvc.dto.DocumentResponseDTO;
 import com.aish.mvc.service.DocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +23,10 @@ public class DocumentController {
     @GetMapping
     public ResponseEntity<List<DocumentResponseDTO>> getAll() {
         return ResponseEntity.ok(documentService.getAllDocuments());
+    }
+    @PostMapping
+    public ResponseEntity<DocumentResponseDTO> create(@RequestBody DocumentRequestDTO request) {
+        DocumentResponseDTO response = documentService.createDocument(request);
+        return ResponseEntity.ok(response);
     }
 }
