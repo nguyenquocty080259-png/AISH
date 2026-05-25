16046e97cd88f36b5597a4ae7ccfe7bc15b11e0f
tranvudinhlang2005 <tranvudinhlang2005@gmail.com>
2026-05-25 10:56:27 +0700

    Step 1: Setup Environment - Compile: SUCCESS
    Installed JDK 21 and verified Maven wrapper compatibility. Progress tracking updated.

--- backend/src/main/java/com/aish/mvc/controller/DocumentController.java
+++ backend/src/main/java/com/aish/mvc/controller/DocumentController.java
@@ -5,6 +5,7 @@ import com.aish.mvc.dto.DocumentResponseDTO;
 import com.aish.mvc.service.DocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/documents")
+@CrossOrigin(origins = "*")
 public class DocumentController {
 
     @Autowired
