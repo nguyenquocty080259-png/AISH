e0aa7e0730deb6db94fc51e68199f767b99ef126
Quoc Ty <nguyenquocty080259@gmail.com>
2026-05-31 23:02:21 +0700

    mấy cái entity đầu

---
+++ backend/src/main/java/com/aish/mvc/entity/DocTag.java
@@ -0,0 +1,24 @@
+package com.aish.mvc.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "doc_tags")
+public class DocTag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 100)
+    @NotNull
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+}
