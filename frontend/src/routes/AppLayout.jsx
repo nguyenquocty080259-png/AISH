a6b04a49cd43a5034b93a967fc92f25960ff2bee
quoc-ty <nguyenquocty080259@gmail.com>
2026-07-14 22:25:12 +0700

    fix: hide AI widget for guests

--- frontend/src/routes/AppLayout.jsx
+++ frontend/src/routes/AppLayout.jsx
@@ -93,7 +93,7 @@ export default function AppLayout() {
           </main>
         </div>
       </div>
-      <AIHiveMindWidget />
+      {isAuthenticated && <AIHiveMindWidget />}
     </AiWidgetProvider>
   );
 }
