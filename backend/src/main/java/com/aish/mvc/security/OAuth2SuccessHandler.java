f9fe394b3f4de6df8fc65fbab143ec1047a02dc3
LamAnhNhu <se193810nguyenthianhnhu@gmail.com>
2026-07-25 02:19:46 +0700

    thêm package util để thêm thông báo của đăng nhập với provider

--- backend/src/main/java/com/aish/mvc/security/OAuth2SuccessHandler.java
+++ backend/src/main/java/com/aish/mvc/security/OAuth2SuccessHandler.java
@@ -6,6 +6,7 @@ import com.aish.mvc.entity.enums.UserStatus;
 import com.aish.mvc.repository.auth.*;
 import com.aish.mvc.service.auth.JwtUtil;
 import com.aish.mvc.service.auth.UsernameGenerator;
+import com.aish.mvc.util.AuthProviderMessageUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -114,29 +115,15 @@ public class OAuth2SuccessHandler
 
         if (existingAccount.isPresent()) {
 
-            String providerMessage;
-
-            switch (existingAccount.get().getProvider()) {
-
-                case LOCAL ->
-                        providerMessage = "Địa chỉ email này được đăng ký bằng Email & Mật khẩu.";
-
-                case GOOGLE ->
-                        providerMessage = "Tài khoản này đã tồn tại. Vui lòng đăng nhập bằng tài khoản Google.";
-
-                case GITHUB ->
-                        providerMessage = "Tài khoản này được đăng ký bằng GitHub.";
-
-                default ->
-                        providerMessage = "Tài khoản này đã tồn tại.";
-            }
+            String providerMessage = AuthProviderMessageUtil.getProviderMessage(
+                    existingAccount.get().getProvider()
+            );
 
             response.sendRedirect(
                     frontendBaseUrl +
                             "/login?error=" +
                             URLEncoder.encode(providerMessage, StandardCharsets.UTF_8)
             );
-
             return;
         }
 
